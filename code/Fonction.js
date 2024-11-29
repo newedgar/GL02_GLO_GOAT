d72d8758d2f23e7e737fbb1aacc342d225f9f5e9
Mathias <mathias.notter@utt.fr>
2024-11-29 17:30:49 +0100

    Ajout du main + fonction getAllCruFiles
    Appel possible de ces 7 commandes
    --find-rooms-by-course <courseId>
    --max-capacity <roomName>
    --room-availability <roomName>
    --available-rooms <day> <time>
    --check-conflicts
    --list-timeslots
    --export-ical <outputFile>

--- code/Fonction.js
+++ code/Fonction.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const { TeachingTimeslot, Calendar } = require('./Classes.js');
 
 function loadDataFromFile(filename) {
@@ -15,7 +16,7 @@ function loadDataFromFile(filename) {
       if (dayTime) {
         const [day, time] = dayTime.split(' ');
         const [startTime, endTime] = time.split('-');
-        
+
         const timeslotObj = new TeachingTimeslot(
           type.trim(),
           parseInt(capacity.replace('P=', '')),
@@ -25,7 +26,7 @@ function loadDataFromFile(filename) {
           subgroupIndex.trim(),
           roomName.replace('S=', '').replace('//', '').trim()
         );
-        
+
         calendar.addTimeslot(timeslotObj);
       }
     }
@@ -34,4 +35,20 @@ function loadDataFromFile(filename) {
   return calendar;
 }
 
-module.exports = { loadDataFromFile };
+// Fonction pour parcourir les sous-dossiers et récupérer les fichiers .cru
+function getAllCruFiles(dir) {
+  const files = [];
+  const items = fs.readdirSync(dir, { withFileTypes: true }); // Liste les fichiers et dossiers
+  for (const item of items) {
+    const fullPath = path.join(dir, item.name);
+    if (item.isDirectory()) {
+      files.push(...getAllCruFiles(fullPath)); // Récursion pour les sous-dossiers
+    } else if (item.isFile() && fullPath.endsWith('.cru')) {
+      files.push(fullPath); // Ajouter les fichiers .cru
+    }
+  }
+  return files;
+}
+
+
+module.exports = { loadDataFromFile, getAllCruFiles };
